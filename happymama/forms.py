220bbec95596fd2c806a98fa89c44822d7adebd5
Hyung Tae C. Figur <hyu_03@hotmail.com>
2020-06-24 16:41:22 -0300

    Cleaning

--- happymama/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    fonetic = forms.CharField()
-    email = forms.EmailField(required=True)
-    phone = forms.CharField()
-    message = forms.CharField(required=True)
